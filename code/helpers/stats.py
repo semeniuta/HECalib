1af636970952e63011a2ebd5cf1324b19957b068
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2014-01-12 12:02:07 +0100

    Some refactoring

---
+++ code/helpers/stats.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+'''
+Compute and display statistics
+'''
+
+import numpy as np
+
+def calc_statistics(sample):
+    res = {}
+    res['mean'] = np.mean(sample)
+    res['min'] = min(sample)
+    res['max'] = max(sample)
+    res['var'] = np.var(sample)
+    res['stdev'] = np.std(sample)
+    
+    return res
+    
+def print_statistics(s):
+    tpl = (s['min'], s['mean'], s['max'], s['var'], s['stdev'])
+    print '%.2f\t%.2f\t%.2f\t%.2f\t%.2f' % tpl
+    
+def print_statistics_header():
+    print 'min\tmean\tmax\tvar\tstdev'
+    
