87b0c1b84c6327cd4d0a74a1a5794f85d3a27eb4
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2014-01-21 11:19:41 +0100

    Edited norm functions in params.py

--- code/params.py
+++ code/params.py
@@ -13,8 +13,9 @@ datadir = r'../data'
 datafiles = [f for f in glob('%s/*.npy' % datadir)]  
 calibrator_classes = [ParkMartinCalibrator, TsaiLenzCalibrator]
 
-#norm_func = lambda M: np.linalg.det(M)    
-#norm_func = lambda M: np.linalg.norm(M)    
-#norm_func = lambda M: np.max(M)
-norm_func = lambda M: np.max(np.abs(M))
+norm_func_det = lambda M: np.linalg.det(M)    
+norm_func_norm = lambda M: np.linalg.norm(M)    
+norm_func_maxabs = lambda M: np.max(np.abs(M))
+
+norm_func = norm_func_maxabs
 
