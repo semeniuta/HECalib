805b35d670043f584ba137bcaa851df20c3c3227
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2014-01-23 10:38:05 +0100

    Modified script_olrem_recalc.py

--- code/script_olrem_recalc.py
+++ code/script_olrem_recalc.py
@@ -10,6 +10,7 @@ import olrem
 from helpers import stats
 from matplotlib import pyplot as plt
 from hecalibrators.park_martin_calibration import ParkMartinCalibrator
+from hecalibrators import calibinteract as ci
 
 def calc_avg_min_max_norms(norms):
     s = stats.calc_statistics(norms)
@@ -19,24 +20,24 @@ if __name__ == '__main__':
     
     datafile = params.datafiles[2]
     pairs, AB, AB_pairs = olrem.read_pairs_and_calc_AB(datafile)
-    res = olrem.process_pairs(pairs, AB, AB_pairs, ParkMartinCalibrator, params.norm_func)
+    old_X = ci.get_calibration_result(pairs, ParkMartinCalibrator)
+    old_matrices, old_norms = olrem.calc_norms(AB, old_X, params.norm_func)
         
     ''' Try Park-Martin calibration with new pairs '''
     top_limit = 0.5
-    filtered_indices = recalc.filter_pairs(res['norms'], lambda x: x < top_limit)
+    filtered_indices = recalc.filter_pairs(old_norms, lambda x: x < top_limit)
     
     pmc = ParkMartinCalibrator(pairs)
     new_X = recalc.recalculate_X(pmc, filtered_indices)
     
     new_matrices, new_norms = olrem.calc_norms(AB, new_X, params.norm_func)
-    old_norms = res['norms']
     
     print '\tavg\tmin\tmax'
     print 'Old:\t%.2f\t%.2f\t%.2f' % calc_avg_min_max_norms(old_norms)   
     print 'New:\t%.2f\t%.2f\t%.2f' % calc_avg_min_max_norms(new_norms)
     
     print '\nMatrix X (old):'
-    print res['X']
+    print old_X
     
     print '\nMatrix X (new):'
     print new_X
