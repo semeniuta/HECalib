2a5ed829d748b2b9d549b1c333462099ef9f1381
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2014-02-17 15:49:15 +0100

    Implmented get_oib_data_pandas

--- code/outliers/precision.py
+++ code/outliers/precision.py
@@ -2,6 +2,7 @@
 
 from helpers import m3dinteract as m3di
 import numpy as np
+import pandas as pd
 import scipy
 
 def calc_object_in_base(R, V, X):
@@ -10,6 +11,10 @@ def calc_object_in_base(R, V, X):
 def get_oib_data(pose_pairs, X):
     oib_list = [m3di.flatten_transform(calc_object_in_base(R, V, X)) for R, V in pose_pairs]
     return np.array(oib_list)
+    
+def get_oib_data_pandas(oib_data):
+    colnames = ['r11', 'r12', 'r13', 'r21', 'r22', 'r23', 'r31', 'r32', 'r33', 'd1', 'd2', 'd3']
+    return pd.DataFrame(oib_data, columns=colnames)
     
 def print_var(oib1, oib2):
     for j in range(9, 12):
