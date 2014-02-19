333d6bb3f700e4a39af38d6c58089b411abdb784
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2014-02-19 18:55:53 +0100

    Created function precision_test

--- code/outliers/precision.py
+++ code/outliers/precision.py
@@ -32,3 +32,27 @@ def get_oib_data_pandas(oib_data):
     '''
     colnames = ['r11', 'r12', 'r13', 'r21', 'r22', 'r23', 'r31', 'r32', 'r33', 'd1', 'd2', 'd3']
     return pd.DataFrame(oib_data, columns=colnames)
+    
+def precision_test(noe, top_limits, components=['d1', 'd2', 'd3']):
+    vars_1 = []    
+    vars_2 = []    
+    
+    for tlim in top_limits:    
+        noe.remove_outliers(tlim)
+    
+        df1 = get_oib_data_pandas(noe.old_object_in_base)
+        df2 = get_oib_data_pandas(noe.new_object_in_base)
+        
+        current_vars_1 = [df1[c].var() for c in components]        
+        current_vars_2 = [df2[c].var() for c in components]        
+        
+        vars_1.append(current_vars_1)
+        vars_2.append(current_vars_2)
+        
+    print 'constructing dataframes'
+    vars_df_1 = pd.DataFrame(np.array(vars_1), columns=components, index=top_limits)
+    vars_df_2 = pd.DataFrame(np.array(vars_2), columns=components, index=top_limits)    
+    
+    return vars_df_1, vars_df_2
+    
+    
--- code/script_olrem_norms.py
+++ code/script_olrem_norms.py
@@ -18,21 +18,10 @@ if __name__ == '__main__':
     
     top_limits = [0.01*i for i in range(34, math.trunc(100*max(noe.old_norms)))]
     
-    target_values = []    
-    for tlim in top_limits:    
-        noe.remove_outliers(tlim)
-    
-        df1 = precision.get_oib_data_pandas(noe.old_object_in_base)
-        df2 = precision.get_oib_data_pandas(noe.new_object_in_base)
-        
-        components = ['d1', 'd2', 'd3']
-        var_diffs = [df1[c].var() - df2[c].var() for c in components]        
-        target = sum(var_diffs)
-        target_values.append(target)
-        print tlim, target
-        
-    plt.figure()
-    plt.plot(top_limits, target_values)
+    v1, v2 = precision.precision_test(noe, top_limits)
+    diff = v1 - v2
+    s = diff['d1'] + diff['d2'] + diff['d3']
+    plt.plot(top_limits, s)
     
     
     
