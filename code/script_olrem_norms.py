e97557b5be37d46e3b507e3aa8159ccce43f71ce
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2014-03-01 14:51:32 +0100

    Customized plots in script_olrem_norms

--- code/script_olrem_norms.py
+++ code/script_olrem_norms.py
@@ -12,15 +12,17 @@ from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     
-    datafile = params.datafiles[2]
+    datafile = params.datafiles[0]
 
     noe = NormsOutliersEliminator(datafile)    
     
     # top_limits = [0.001*i for i in range(500, math.trunc(1000*max(noe.old_norms)))]    
     top_limits = [0.01*i for i in range(50, math.trunc(100*max(noe.old_norms)))]
     
+    ''' Variances '''    
     v1, v2 = precision.precision_test(noe, top_limits)
 
+    ''' Cumulative varinces '''
     s1 = v1['d1'] + v1['d2'] + v1['d3']
     s2 = v2['d1'] + v2['d2'] + v2['d3']
 
@@ -28,10 +30,16 @@ if __name__ == '__main__':
         plt.figure()
         plt.plot(top_limits, v1[d], label='old var(%s)' % d)
         plt.plot(top_limits, v2[d], label='new var(%s)' % d)
+        plt.legend()
+        plt.xlabel('Value of norm_min')
+        plt.ylabel('Varince of %s parameter' % d)
         
     plt.figure()
-    plt.plot(top_limits, s1, label='s1')
-    plt.plot(top_limits, s2, label='s2')
+    plt.plot(top_limits, s1, label='Sum of variances before removing outliers')
+    plt.plot(top_limits, s2, label='Sum of variances after removing outliers')
+    plt.legend()
+    plt.xlabel('Value of norm_min')
+    plt.ylabel('Sum of varinces')
               
     
     
