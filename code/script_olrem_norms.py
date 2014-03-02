18b6728e63440c19327f447ea0419c138601b29d
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2014-03-02 15:08:25 +0100

    Now working with the norm_func_norm function

--- code/script_olrem_norms.py
+++ code/script_olrem_norms.py
@@ -12,9 +12,10 @@ from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     
-    datafile = params.datafiles[0]
+    datafile = params.datafiles[2]
 
     noe = NormsOutliersEliminator(datafile)    
+    print noe.old_norms    
     
     # top_limits = [0.001*i for i in range(500, math.trunc(1000*max(noe.old_norms)))]    
     top_limits = [0.01*i for i in range(50, math.trunc(100*max(noe.old_norms)))]
@@ -26,20 +27,42 @@ if __name__ == '__main__':
     s1 = v1['d1'] + v1['d2'] + v1['d3']
     s2 = v2['d1'] + v2['d2'] + v2['d3']
 
+    index_min = s2.index[s2.argmin()]
+    
+    '''
     for d in ['d1', 'd2', 'd3']:
         plt.figure()
         plt.plot(top_limits, v1[d], label='old var(%s)' % d)
         plt.plot(top_limits, v2[d], label='new var(%s)' % d)
         plt.legend()
-        plt.xlabel('Value of norm_min')
+        plt.xlabel('Value of threshold')
         plt.ylabel('Varince of %s parameter' % d)
+        plt.vlines(index_min, plt.ylim()[0], plt.ylim()[1], color='r')
+        print plt.ylim()[0], plt.ylim()[1]
+    #'''
+        
+    colors = ['b', 'g', 'c']
+    components = ['d1', 'd2', 'd3']
+    plt.figure()
+    for i in range(len(components)):
+        d = components[i]
+        c = colors[i]
+        plt.plot(top_limits, v1[d], label='old var(%s)' % d, color=c)
+        plt.plot(top_limits, v2[d], label='new var(%s)' % d, color=c)
+    plt.legend()
+    plt.xlabel('Value of threshold')
+    plt.ylabel('Varince')
+    plt.vlines(index_min, plt.ylim()[0], plt.ylim()[1], color='r')
+    print plt.ylim()[0], plt.ylim()[1]
         
     plt.figure()
     plt.plot(top_limits, s1, label='Sum of variances before removing outliers')
     plt.plot(top_limits, s2, label='Sum of variances after removing outliers')
     plt.legend()
-    plt.xlabel('Value of norm_min')
+    plt.xlabel('Value of threshold')
     plt.ylabel('Sum of varinces')
+    plt.vlines(index_min, plt.ylim()[0], plt.ylim()[1], color='r')
+    print plt.ylim()[0], plt.ylim()[1]
               
     
     
